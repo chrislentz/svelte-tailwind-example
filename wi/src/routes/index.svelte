a89cbb8a70d3b8ec590f70b3c56421340d4ed597
Chris Lentz <chris@atlchris.com>
2022-02-19 20:57:41 -0500

    Added Svelte preprocessor support

--- wi/src/routes/index.svelte
+++ wi/src/routes/index.svelte
@@ -5,3 +5,7 @@
 <div class="h-full flex items-center justify-center">
 	<h1 class="center text-5xl text-red-800">Welcome to SvelteKit</h1>
 </div>
+
+<style lang="postcss">
+	@apply md:text-green-800;
+</style>
